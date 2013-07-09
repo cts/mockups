32b78c3483d5e24ef93324da077a1c74a5681ee6
Jessica Andersen <jessmand@mit.edu>
2013-07-09 11:12:08 -0400

    testing new list generator

---
+++ blog/_jekyll/_plugins/_list_gen.rb
@@ -0,0 +1,56 @@
+module Jekyll
+  class CatsAndTags < Generator
+    def generate(site)
+      site.categories.each do |category|
+        build_subpages(site, "category", category[0], category[1])
+      end
+      
+      site.tags.each do |tag|
+        build_subpages(site, "tag", tag[0], category[1])
+      end
+      years = {};
+      site.posts.each do |post|
+        if years.has_key?(post.date.year)
+          years[post.date.year][post.date.month].push(post)
+          
+        else
+          if (not years[post.date.year].include?(post.date.month)) {
+            years[post.date.year] = {post.date.month=>[post]}
+          }
+        end
+        
+      end
+      years.each do |year|
+        year.each.do |month|
+          build_subpages(site, "archives", "#{year}/#{month}", year[month])
+        end
+      end
+    end
+      
+    def build_subpages(site, type, val, posts)    
+      atomize(site, type, val, posts)
+    end
+      
+    def atomize(site, type, val, posts)
+      path = "/#{type}/#{val}"
+      atom = AtomPage.new(site, site.source, path, type, val, posts)
+      site.pages << atom
+    end 
+    
+    class AtomPage < Page
+      def initialize(site, base, dir, type, val, posts)
+        @site = site
+        @base = base
+        @dir = dir
+        @name = 'index.html'
+    
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), "list.html")
+        self.data[type] = val
+        self.data["type"] = type
+        self.data["posts"] = posts
+      end
+    end
+    
+  end
+end
